a89b42653d41c648604c503b585786c260922287
marziyenajafzade <Marziyenajafzade@gmail.com>
2023-12-19 16:54:13 +0330

    add new style as object

--- src/Components/InlineCss.jsx
+++ src/Components/InlineCss.jsx
@@ -1,20 +1,33 @@
+import React, { useState } from "react";
 
-const InlineCss = () =>{
-    const items = [
-        {id:1, name : 'text1'},
-        {id:2, name : 'text2'},
-        {id:3, name : 'text3'},
-        {id:4, name : 'text4'}
-    ];
+const InlineCss = () => {
+  const items = [
+    { id: 1, name: 'text1' },
+    { id: 2, name: 'text2' },
+    { id: 3, name: 'text3' },
+    { id: 4, name: 'text4' }
+  ];
+
+  const [selected, setSelected] = useState(true);
+
+  const toggleStyle = () => {
+    setSelected((s) => !s);
+  };
+  const cssStyle={marginTop:'10px', color:'red',border:'1px dashed'};
+  const cssStyleClass={marginTop:'10px', color:'blue',border:'1px dashed'}
+
   return (
-   <ul>
-    {items.map((item)=>(
-        <li key={item.id}>{item.name}</li>
-    )
-    
-    )}
-   </ul>
+    <div>
+      <button onClick={toggleStyle}>Change Style</button>
+      <ul >
+        {items.map((item) => (
+          <li key={item.id} style={selected ? cssStyle : cssStyleClass}>{item.name}
+          </li>
+          
+        ))}
+      </ul>
+    </div>
   );
-}
+};
 
-export default InlineCss
+export default InlineCss;
