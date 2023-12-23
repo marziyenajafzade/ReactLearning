5eeced913d1d049e2463cb45e7bc2fb9ecb6df87
marziyenajafzade <Marziyenajafzade@gmail.com>
2023-12-23 11:30:02 +0330

    using styled components with props

--- src/Components/StyledComponents.jsx
+++ src/Components/StyledComponents.jsx
@@ -1,19 +1,35 @@
-import styled from "styled-components"
+import { useState } from "react";
+import styled, { createGlobalStyle } from "styled-components"
 
 const DIV = styled.div`
+color: orange;
 &:hover{
     color : red;
-font-weight : bold;
-font-size : large
+    font-weight : bold;
+     font-size : large
 }
 `;
+const SecondDiv = styled.div`
+  font-size: large;
+  color: ${props => (props.selected ? "blue" : "gray")};
+`;
+
 const StyledComponents = () => {
+    const [selected, setSelected] = useState(true)
     return (
+        <>
+         { /*styled components */}
         <DIV>
             <h1>
                 StyledComponents
             </h1>
         </DIV>
+         { /* styled components with props */}
+        <SecondDiv selected={selected}>
+            <button onClick={()=>setSelected((s)=> !s)}>Change</button>
+            <span>test styled componets with props</span>
+        </SecondDiv>
+        </>
     )
 }
 
